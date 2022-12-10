c1ba1286ca0d5e77b95a5e75bc9ccc1cb282fdf1
ihasansub <106838298+ihasansub@users.noreply.github.com>
2022-12-10 08:22:21 +0200

    General code update

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ class Student:
         self.id = Student.id
 
 
-
 while True:
+    print("(1) New Students\n(2) Edit Student\n(3) Delete Student\n(4) Get Students\n(5) Get Student's Details\n(6)Exit")
     action = input("enter action")
     if action == "1":
         full_name = input("enter full name")
         age = input("enter age")
         level = input("level")
+        if not level.capitalize() in ["A", "B", "C"]:
+            print("PLease choose level from (A, B, C)")
+            level = input("enter level")
         mobile_number = input("mobile number")
         data = {
             "full_name": full_name,
@@ -35,6 +38,9 @@ while True:
         full_name = input("enter full name")
         age = input("enter age")
         level = input("level")
+        if not level.capitalize() in ["A", "B", "C"]:
+            print("PLease choose level from (A, B, C)")
+            level = input("enter level")
         mobile_number = input("mobile number")
 
         data = {
@@ -48,19 +54,20 @@ while True:
 
     elif action == "3":
         std_id = input("enter student id")
-        response = requests.get("http://staging.bldt.ca/api/method/build_it.test.get_student_details", params={"id":std_id})
+        response = requests.get("http://staging.bldt.ca/api/method/build_it.test.get_student_details",
+                                params={"id": std_id})
         response = response.text
         response = json.loads(response)
-        if response['status'] == False:
+        if not response['status']:
             print("There is no student with given id")
             continue
         else:
-            requests.delete("http://staging.bldt.ca/api/method/build_it.test.delete_student", params = {"id": std_id})
+            requests.delete("http://staging.bldt.ca/api/method/build_it.test.delete_student", params={"id": std_id})
 
     elif action == "4":
         response = requests.get("http://staging.bldt.ca/api/method/build_it.test.get_students")
         data = response.text
-        data =json.loads(data)
+        data = json.loads(data)
 
         for std in data['data']:
             f = open("students.txt", "a")
@@ -69,7 +76,7 @@ while True:
 
     elif action == "5":
         std_id = input("enter std id")
-        params ={
+        params = {
             "id": std_id
         }
         response = requests.get("http://staging.bldt.ca/api/method/build_it.test.get_student_details", params)
@@ -80,4 +87,8 @@ while True:
         f.close()
 
     elif action == "6":
+        print("Have a Nice Day!")
         break
+
+    else:
+        print("please choose action from 1-6")
